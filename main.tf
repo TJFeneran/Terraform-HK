6453e11a4c24656cc27dd204008fe20af0389a3c
TJF <tjfeneran@gmail.com>
2024-09-20 20:08:24 -0400

    clean up

--- main.tf
+++ main.tf
@@ -45,10 +45,9 @@ resource "aws_vpc" "main" {
 // Public Subnets
 resource "aws_subnet" "public_subnets" {
 
-  count      = length(var.public_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.public_subnet_cidrs, count.index)
-  //availability_zone = element(local.availability_zones, count.index)
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
@@ -59,10 +58,9 @@ resource "aws_subnet" "public_subnets" {
 // Private Subnets
 resource "aws_subnet" "private_subnets" {
 
-  count      = length(var.private_subnet_cidrs)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.private_subnet_cidrs, count.index)
-  //availability_zone = element(local.availability_zones, count.index)
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
