14098826b3440c8a9eb0278275d1c47822206ab8
TJF <tjfeneran@gmail.com>
2024-09-24 21:52:02 -0400

    updated aurora-single to use common_vars

--- aurora-single/variables.tf
+++ aurora-single/variables.tf
@@ -1,7 +1,6 @@
-variable "workload_name" {
-  description = "Workload Name"
-  type        = string
-  default     = ""
+variable "default_vars" {
+  description = "Contains all default variables from root common_vars.yaml"
+  type        = any
 }
 
 variable "region" {
@@ -11,7 +10,7 @@ variable "region" {
 
   validation {
     condition     = length(var.region) > 0
-    error_message = "Variable 'region' is missing or invalid. Remember to use a var-file."
+    error_message = "Variable 'region' is missing or invalid. Remember to use a .tfvar file with 'region' variable"
   }
 }
 
@@ -25,4 +24,28 @@ variable "cluster_name" {
   description = "Name of cluster"
   type        = string
   default     = "cluster"
+}
+
+variable "storage_type" {
+  description = "instance storage type"
+  type        = string
+  default     = ""
+}
+
+variable "db_cluster_instance_class" {
+  description = "Family & size of instance(s) deployed to cluster"
+  type        = string
+  default     = ""
+}
+
+variable "iops" {
+  description = "Storage IOPS per instance"
+  type        = number
+  default     = 0
+}
+
+variable "allocated_storage" {
+  description = "Size (in Gb) for cluster instances"
+  type        = number
+  default     = 0
 }
---
+++ aurora-single/vars1.tfvars
@@ -0,0 +1,7 @@
+/////////////////////////////////////////////////////////////
+// TF VARS FILE to load during runtime of aurora-single
+/////////////////////////////////////////////////////////////
+
+cluster_name = "tfvar-cluster" // give this a proper name for its purpose
+region       = "us-east-2"     // region to deploy to
+instances    = 1               // number of instances to deploy across AZs
